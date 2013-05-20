15fc6a317aa74eda5bf97fbeee5a3882f1a7024a
MJ O'Carroll <ocarrollmj@gmail.com>
2013-05-20 17:59:36 +0100

    test

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -24,7 +24,7 @@ end
                     format:     { with: VALID_EMAIL_REGEX },
                     uniqueness: { case_sensitive: false }
   validates :password, presence: true, length: { minimum: 6 }
-=end  validates :password_confirmation, presence: true
+=end  
 
 def self.search(params)
   
